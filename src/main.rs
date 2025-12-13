d803cb786bdf22fe3795521d1e44f01a1d9529c9
codewithsilva <codewithsilva@gmail.com>
2025-12-13 10:32:09 -0300

    wallet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[derive(Debug)]
+struct CdiEvent {
+  description:String,
+  amount:i64,
+}
+
+#[derive(Debug)]
+struct Wallet {
+  owner:String,
+  balance:i64,
+  cdi_events: Vec<CdiEvent>
+}
+
 fn main() {
-    println!("Hello, world!");
+  let wallet = Wallet {
+    owner: "Silva".to_string(),
+    balance:0, 
+  };
+
+  println!("Balance: {:?}, Owner: {:?}", wallet.balance, wallet.owner);
 }
